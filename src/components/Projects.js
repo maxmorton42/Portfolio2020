fcd3f5c24ba5a5a89325f52bd515e07278f2b8fa
Max Morton <max.morton.42@gmail.com>
2020-01-02 09:39:47 -0700

    present class

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,14 @@ const Projects = () => {
     <>
     <h1>Projects</h1>
       <hr />
+      <h2>FaceBook App</h2>
+      <p>React application with Rails Backend, utilizes Devise Authorization with a working token system </p>
+        <a href="https://friend--book.herokuapp.com/"> FaceBook - Hosted with Heroku </a>
+        <br /> <br />
       <h2>JavaScript FlashCards</h2>
       <p>Basic flashcard app which allows you to add or remove new flashcards, 
         since it only uses JS the data does not persist </p>
-        <a href="https://fervent-blackwell-384c02.netlify.com"> FlashCard App </a>
+        <a href="https://fervent-blackwell-384c02.netlify.com"> FlashCard App -Hosted with Netlify </a>
     </>
   )
 }
